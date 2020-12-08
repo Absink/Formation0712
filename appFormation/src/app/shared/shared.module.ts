9a8678dff2d431745576fdf70d339f9f6ba54fb5
Olivier <o.blaivie@coperbee.fr>
2020-12-08 16:27:50 +0100

    Ajout du pipe

--- appFormation/src/app/shared/shared.module.ts
+++ appFormation/src/app/shared/shared.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableLightComponent } from './components/table-light/table-light.component';
+import { TotalPipe } from './pipes/total.pipe';
+import { DaysPipe } from './pipes/days.pipe';
+import { Days2Pipe } from './pipes/days2.pipe';
 
 
 
 @NgModule({
-  declarations: [TableLightComponent],
+  declarations: [TableLightComponent, TotalPipe, DaysPipe, Days2Pipe],
   imports: [
     CommonModule
   ],
   exports: [
-    TableLightComponent
+    TableLightComponent,
+    TotalPipe
   ]
 })
 export class SharedModule { }
