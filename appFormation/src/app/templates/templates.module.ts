230e6b4235ab86e19b0202c6d5398859b510bcae
Olivier <o.blaivie@coperbee.fr>
2020-12-10 12:30:28 +0100

    Ajout global text

--- appFormation/src/app/templates/templates.module.ts
+++ appFormation/src/app/templates/templates.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TitresAComponent } from './titres-a/titres-a.component';
 import { TitresBComponent } from './titres-b/titres-b.component';
+import { TextsModule } from '../texts/texts.module';
 
 
 
 @NgModule({
   declarations: [TitresAComponent, TitresBComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TextsModule
   ],exports: [
     TitresAComponent,
     TitresBComponent
